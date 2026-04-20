17b2b7c4b741fd956ec72c0d93f679efc1e028a5
ahcene <ahcenenekali@gmail.com>
2026-04-19 17:49:13 -0700

    Implémentation de la couche Service - logique métier

--- main.py
+++ main.py
@@ -3,15 +3,43 @@ from models.salle import Salle
 
 dao = DataSalle()
 
-# Test connexion
+
 print("Connexion OK")
 
-# Ajouter une salle
 s = Salle("A101", "Salle informatique", "Laboratoire", 30)
 dao.insert_salle(s)
 print("Salle ajoutée")
 
-# Afficher toutes les salles
+
 salles = dao.get_salles()
 for salle in salles:
     salle.afficher_infos()
+    from services.services_salle import ServiceSalle
+    from models.salle import Salle
+
+    service = ServiceSalle()
+
+
+    s = Salle("B202", "Salle de classe", "Classe", 25)
+    resultat, message = service.ajouter_salle(s)
+    print(message)
+
+
+    salles = service.recuperer_salles()
+    for salle in salles:
+        salle.afficher_infos()
+        print("---")
+
+
+    s = service.rechercher_salle("B202")
+    if s:
+        s.afficher_infos()
+
+
+    s.capacite = 40
+    resultat, message = service.modifier_salle(s)
+    print(message)
+
+
+    service.supprimer_salle("B202")
+    print("Salle supprimée")
---
+++ services/services_salle.py
@@ -0,0 +1,32 @@
+from Data.dao_salle import DataSalle
+from models.salle import Salle
+
+class ServiceSalle:
+
+    def __init__(self):
+        self.dao_salle = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1"
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutée avec succès"
+
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1"
+        self.dao_salle.update_salle(salle)
+        return True, "Salle modifiée avec succès"
+
+    def supprimer_salle(self, code):
+        self.dao_salle.delete_salle(code)
+
+    def rechercher_salle(self, code):
+        return self.dao_salle.get_salle(code)
+
+    def recuperer_salles(self):
+        return self.dao_salle.get_salles()
